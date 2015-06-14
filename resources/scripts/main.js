3810de250f511afd1d8d51695609cd7a5610974d
amir farzan <amir.ff@gmail.com>
2015-06-14 14:49:07 -0700

    home page start

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -3,4 +3,23 @@ $(document).ready(function(){
     $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
     e.preventDefault();
   });
+
+  $(function() {
+    $("#modal-1").on("change", function() {
+      if ($(this).is(":checked")) {
+        $("body").addClass("modal-open");
+      } else {
+        $("body").removeClass("modal-open");
+      }
+    });
+
+    $(".modal-fade-screen, .modal-close").on("click", function() {
+      $(".modal-state:checked").prop("checked", false).change();
+    });
+
+    $(".modal-inner").on("click", function(e) {
+      e.stopPropagation();
+    });
+  });
+
 });
